9aa2b62a2332a670602bb31fa3a9155fb5f7ce6a
Sam Julien <spjulien@gmail.com>
2020-05-06 12:37:15 -0700

    Add SEO to page component

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { css } from '@emotion/core'
 import Layout from '../components/Layout'
 import Container from 'components/Container'
+import SEO from 'components/SEO/'
 
 export default ({ children }) => {
   return (
     <Layout>
+      <SEO />
       <Container
         css={css`
           padding-bottom: 0;
