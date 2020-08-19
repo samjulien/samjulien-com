c513502273b7559051b57ea5c61cf82ae2fce578
Sam Julien <spjulien@gmail.com>
2020-08-19 16:47:50 -0700

    Fix purge css config

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,6 +120,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: false, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
+        whitelist: ['-my-5'],
         ignore: ['index.mdx'],
       },
     },
