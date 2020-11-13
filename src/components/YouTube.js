df8b6f766619ced8b77032e3fbff8a1b1cc935da
Sam Julien <spjulien@gmail.com>
2020-11-13 13:05:28 -0800

    Add Collab Lab talk to Learning System page

--- src/components/YouTube.js
+++ src/components/YouTube.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Video from './Video'
 
-const YouTube = ({ shortCode }) => {
+const YouTube = ({ shortCode, start }) => {
+  const query = start ? `?start=${start}` : ''
   return (
     <Video
-      src={`https://www.youtube.com/embed/${shortCode}`}
+      src={`https://www.youtube.com/embed/${shortCode}${query}`}
       frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
